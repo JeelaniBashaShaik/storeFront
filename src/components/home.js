ff7e0c098f97efa4be18e2f8322fcfd87795fed3
JeelaniBashaShaik <jeelanshaik07@gmail.com>
2021-05-24 16:34:14 +0530

    1 file modified, 1 added, 1 deleted

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import LoginPage from './../containers/login';
+import storeImage from './../assets/images/store-adhoc.png';
 
 export const Home = () => {
   return (
     <React.Fragment>
-      <LoginPage />
+      <div class="home">
+        <div>
+          <img src={storeImage} alt="store image" />
+        </div>
+      </div>
     </React.Fragment>
   );
 };
